9934786a3158ed1675b2d95b3c194d1aece3b8b7
kazilop <rybka.artem@gmail.com>
2022-01-20 15:00:25 +0300

    Setup project
    fixed movement
    add buttons

---
+++ Assets/Scripts/Queen.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queen : MonoBehaviour
+{
+    private float playerSpeed = 2.0f;
+    private Vector3 move;
+    public float speed = 10f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       // controller = gameObject.AddComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        move = Vector3.zero;
+    }
+
+    // Update is called once per frame
+   
+
+    private void FixedUpdate()
+    {
+        MovementLogic();
+    }
+
+    private void MovementLogic()
+    {
+
+        rb.AddForce(move * speed);
+    }
+
+    public void MoveForward()
+    {
+        move = new Vector3(0,0,playerSpeed);
+    }
+
+    public void MoveBack()
+    {
+        move = new Vector3(0,0,- playerSpeed);
+    }
+
+    public void MoveLeft()
+    {
+        move = new Vector3(-playerSpeed, 0,0);
+    }
+
+    public void MoveRight()
+    {
+        move = new Vector3(playerSpeed, 0,0);
+    }
+
+    public void MoveStop()
+    {
+        move = Vector3.zero;
+    }
+}
+
+  
